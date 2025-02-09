8612fd15510c99370734c6f89f7ff803f43c8bf6
muex <a.mueller@muex.de>
2025-02-09 19:25:26 +0100

    fix error and use filetype

--- src/Form/RecipeType.php
+++ src/Form/RecipeType.php
@@ -6,6 +6,7 @@ use App\Entity\Recipe;
 use App\Entity\User;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
